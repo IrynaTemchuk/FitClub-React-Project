1f8e808efc5ab038ac775cd1ce6ad49d53fb463d
Iryna Temchuk <itemchuk98@gmail.com>
2023-04-10 18:23:57 +0300

    Fix import logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import Logo from '../../assets/logo.png'
 const Header = () => {
   return (
     <div className='header'>
-      <img src="{Logo}" alt="" className='logo'/>
+      <img src={Logo} alt="" className='logo'/>
       <ul className='header-menu'>
         <li>Home</li>
         <li>Programs</li>
