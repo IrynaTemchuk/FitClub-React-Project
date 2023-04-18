99a97e1286066a95190512ad2e0085f2c196fa58
Iryna Temchuk <itemchuk98@gmail.com>
2023-04-18 10:45:59 +0300

    Add join section with styles and send email

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-import './Join.css'
+import React, { useRef } from 'react';
+import './Join.css';
+import emailjs from '@emailjs/browser'
 
 
 const Join = () => {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_mave8eq', 'template_w9f3xtb', form.current, 'TIFcou0s4-N1Z94Hr')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
   return (
     <div className="Join" id="join-us">
         {/* left side join */}
@@ -10,16 +25,20 @@ const Join = () => {
             <hr />
             <div>
                 <span className="stroke-text">READY TO</span>
-                <span>LEVEL UP</span>
+                <span> LEVEL UP</span>
             </div>
             <div>
                 <span>YOUR BODY</span>
-                <span className="stroke-text">WITH US?</span>
+                <span className="stroke-text"> WITH US?</span>
             </div>
         </div>
+
         {/* right side join */}
         <div className="right-j">
-
+          <form ref={form} className="email-container" onSubmit={sendEmail}>
+            <input type="email" name="user_email" placeholder="Enter your Email address" />
+            <button className="btn btn-j">Join Now</button>
+          </form>
         </div>
     </div>
   )
